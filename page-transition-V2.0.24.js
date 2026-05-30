984ea3298328b96bf1a4d4e2f6dce3fcecdda1cc
AVOSSSigmaTeam <113782378+AVOSSSigmaTeam@users.noreply.github.com>
2026-05-30 11:06:33 +0200

    plan popup element get change

--- page-transition-V2.0.24.js
+++ page-transition-V2.0.24.js
@@ -1,6 +1,6 @@
 gsap.registerPlugin(CustomEase, ScrollTrigger);
 
-const version = "2.0.23";
+const version = "2.0.24";
 
 history.scrollRestoration = "manual";
 
@@ -822,11 +822,17 @@ function initSkipLink() {
 
 //plan popup
 function initPlanPopup(page) {
-  const popupTriggers = page.querySelectorAll("[data-plan-popup-trigger]");
-  const popupCloseTriggers = page.querySelectorAll("[data-popup-close-trigger]");
-  const popupWrapper = page.querySelector("[data-plan-popup-wrapper]");
-  const allPopups = Array.from(page.querySelectorAll("[data-plan-popup]"));
-  const blurTargets = page.querySelectorAll(".popup-background-blur");
+  // const popupTriggers = page.querySelectorAll("[data-plan-popup-trigger]");
+  // const popupCloseTriggers = page.querySelectorAll("[data-popup-close-trigger]");
+  // const popupWrapper = page.querySelector("[data-plan-popup-wrapper]");
+  // const allPopups = Array.from(page.querySelectorAll("[data-plan-popup]"));
+  // const blurTargets = page.querySelectorAll(".popup-background-blur");
+
+  const popupTriggers = page.querySelectorAll("[data-plan-popup-trigger]") || document.querySelectorAll("[data-plan-popup-trigger]");
+  const popupCloseTriggers = page.querySelectorAll("[data-popup-close-trigger]") || document.querySelectorAll("[data-popup-close-trigger]");
+  const popupWrapper = page.querySelector("[data-plan-popup-wrapper]") || document.querySelector("[data-plan-popup-wrapper]");
+  const allPopups = Array.from(page.querySelectorAll("[data-plan-popup]")) || Array.from(document.querySelectorAll("[data-plan-popup]"));
+  const blurTargets = page.querySelectorAll(".popup-background-blur") || document.querySelectorAll(".popup-background-blur");
 
   if (popupTriggers.length === 0) {
     if (DEBUG) console.warn("No plan popup triggers found");
@@ -841,15 +847,15 @@ function initPlanPopup(page) {
     if (DEBUG) console.warn("No plan popups found");
   }
 
-  // if (!popupTriggers.length || !popupWrapper || !popupCloseTriggers.length || !allPopups.length) {
-  //   if (DEBUG) console.warn("Plan popup elements not found", {
-  //     popupTriggers,
-  //     popupWrapper,
-  //     popupCloseTriggers,
-  //     allPopups
-  //   });
-  //   return;
-  // }
+  if (!popupTriggers.length || !popupWrapper || !popupCloseTriggers.length || !allPopups.length) {
+    // if (DEBUG) console.warn("Plan popup elements not found", {
+    //   popupTriggers,
+    //   popupWrapper,
+    //   popupCloseTriggers,
+    //   allPopups
+    // });
+    return;
+  }
 
   // Prevent duplicate listeners if this container is initialized again
   if (popupWrapper._planPopupCleanup) popupWrapper._planPopupCleanup();
